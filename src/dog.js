ade68ef83cdbd834c90bab7a1466e0aea810b4d9
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2017-04-15 13:13:36 -0400

    initial from end of step 2

---
+++ src/dog.js
@@ -0,0 +1,16 @@
+// @flow
+
+class Dog {
+    name: string
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    bark() {
+        return `Wah wah, I am ${this.name}`;
+    }
+}
+
+
+export default Dog;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+// @flow
+
+import Dog from './dog';
+
+
+const toby = new Dog('Toby');
+console.log(toby.bark());
